d261349bf85abee6fc48c41571e0cacebe34639a
hallouma875 <ahlem.fahem98@gmail.com>
2020-12-16 18:07:42 +0100

    add dao and controllers folders

--- BibliothequeProject/src/main/java/com/catho/bibliothequeProject/Controllers/UtilisateurController.java
+++ BibliothequeProject/src/main/java/com/catho/bibliothequeProject/Controllers/UtilisateurController.java
@@ -1,13 +1,5 @@
 package com.catho.bibliothequeProject.controllers;
 
-import com.catho.bibliothequeProject.entity.Utilisateur;
-import com.catho.bibliothequeProject.Dao.UtilisateurDao;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
 import java.util.Optional;
 import java.util.Set;
 
