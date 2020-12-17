5096ee98096f2593ebec8c77f277f6a0c79febf8
lamya-rey <61109480+lamya-rey@users.noreply.github.com>
2020-12-18 00:05:34 +0100

    modif usercontroller + data.sql

--- BibliothequeProject/src/main/java/com/catho/bibliothequeProject/controller/UtilisateurController.java
+++ BibliothequeProject/src/main/java/com/catho/bibliothequeProject/controller/UtilisateurController.java
@@ -2,9 +2,14 @@ package com.catho.bibliothequeProject.controller;
 
 import com.catho.bibliothequeProject.dao.UserRepository;
 import com.catho.bibliothequeProject.entity.User;
+import com.catho.bibliothequeProject.entity.User;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -23,5 +28,25 @@ public class UtilisateurController {
     @GetMapping("/{userId}")
     public Optional<User> getUserById(@PathVariable Long userId) {
         return userRepository.findById(userId);
+    }
+    
+    @GetMapping("/{name}")
+    public User findByName(@PathVariable String name) {
+    	return userRepository.findByTName(name);
+    }
+    
+    @DeleteMapping("/")
+    public void  deleteById(@PathVariable Long id){
+    	userRepository.deleteById(id);
+    }
+    
+    @PostMapping("/")
+    public User save(@RequestBody User user) {
+    	return userRepository.save(user);
+    }
+    
+    @PutMapping("/")
+    public User update(@RequestBody User user) {
+    	return userRepository.saveAndFlush(user);
     }
 }
--- BibliothequeProject/src/main/resources/data.sql
+++ BibliothequeProject/src/main/resources/data.sql
@@ -1,24 +1,24 @@
 
 
-insert into user (id, Name,age, email, userName, nbrEmpr) values (1,'amira','28','amirafahem1920@gmail.com','amira_fahem', 15);
-insert into user (id, Name,age, email, userName, nbrEmpr) values (2,'ahlem','22','ahlem.fahem98@gmail.com','ahlem123',10);
-insert into user (id, Name,age, email, userName,nbrEmpr)values (3,'lamya','22','lamya.rayess1@gmail.com','lamia.rayess', 5);
-insert into user (id, Name,age, email, userName,nbrEmpr) values (4,'hanen','23','hanen_mansour@gmail.com','hanen1997', 35);
-insert into user (id, Name,age, email, userName, nbrEmpr) values (5,'ines','20','ines.loulou123@gmail.com','inesnoussa',20);
+insert into user (id, Name,age, email, userName, nbrEmpr) values (1,'amira','15','amirafahem1920@gmail.com','amira_fahem', 1);
+insert into user (id, Name,age, email, userName, nbrEmpr) values (2,'ahlem','22','ahlem.fahem98@gmail.com','ahlem123', 1);
+insert into user (id, Name,age, email, userName,nbrEmpr)values (3,'lamya','23','lamya.rayess1@gmail.com','lamia.rayess', 1);
+insert into user (id, Name,age, email, userName,nbrEmpr) values (4,'hanen','17','hanen_mansour@gmail.com','hanen1997', 1);
+insert into user (id, Name,age, email, userName, nbrEmpr) values (5,'ines','10','ines.loulou123@gmail.com','inesnoussa', 1);
 
-insert into books (book_id, title, isbn, category, user_id) values (1,'the fault in our stars', '2-7654-1005-4', 'Amour', 3);
-insert into books (book_id, title, isbn, category, user_id) values (2,'citadel', '4-7144-1775-4', 'Aventure', 1);
-insert into books (book_id, title, isbn, category, user_id) values (3,'Le chat Tome', '3-0054-1105-4', 'enfants', 5);
-insert into books (book_id, title, isbn, category, user_id) values (4,'Bla bla bla Tome', '4-7004-1111-5', 'humour', 4);
-insert into books (book_id, title, isbn, category, user_id) values (5,'magnificat', '3-0054-4422-1', 'classique', 2);
-insert into books (book_id, title, isbn, category, user_id) values (6,'Chonchon', '1-8854-2545-3', 'jeunesse', 5);
-insert into books (book_id, title, isbn, category, user_id) values (7,'La tarentule', '2-9874-8755-7', 'comics', 3);
-insert into books (book_id, title, isbn, category, user_id) values (8,'Darkness Tome', '7-1024-9855-2', 'policier', 1);
-insert into books (book_id, title, isbn, category, user_id) values (9,'Ramsès Tome IV : La dame Abou Simbel', '8-3524-1115-1', 'historique', 5);
-insert into books (book_id, title, isbn, category, user_id) values (10,'Le Masque', '6-9204-0005-4', 'Cinéma', 2);
+insert into books (book_id, title, isbn, category, user_id) values (1,'the fault in our stars', '2-7654-1005-4', 'adulte', 3);
+insert into books (book_id, title, isbn, category, user_id) values (2,'citadel', '4-7144-1775-4', 'enfant', 5);
+insert into books (book_id, title, isbn, category, user_id) values (3,'Le chat Tome', '3-0054-1105-4', 'enfant', 0);
+insert into books (book_id, title, isbn, category, user_id) values (4,'Bla bla bla Tome', '4-7004-1111-5', 'ado', 4);
+insert into books (book_id, title, isbn, category, user_id) values (5,'magnificat', '3-0054-4422-1', 'adulte', 2);
+insert into books (book_id, title, isbn, category, user_id) values (6,'Chonchon', '1-8854-2545-3', 'ado', 1);
+insert into books (book_id, title, isbn, category, user_id) values (7,'La tarentule', '2-9874-8755-7', 'adulte', 0);
+insert into books (book_id, title, isbn, category, user_id) values (8,'Darkness Tome', '7-1024-9855-2', 'enfant', 0);
+insert into books (book_id, title, isbn, category, user_id) values (9,'La dame Abou Simbel', '8-3524-1115-1', 'ado', 0);
+insert into books (book_id, title, isbn, category, user_id) values (10,'Le Masque', '6-9204-0005-4', 'adulte', 0);
 
-insert into comments (comment_id, content, title, book_id, user_id) values (1,'seems Great', 'motivated');
-insert into comments (comment_id, content, title, book_id, user_id) values (2,'i Like it', 'grateful');
-insert into comments (comment_id, content, title, book_id, user_id) values (3,'Amazing book', 'Avis');
-insert into comments (comment_id, content, title, book_id, user_id) values (4, 'i dislike its too long', 'too long');
-insert into comments (comment_id, content, title, book_id, user_id) values (5,'Good book', 'Avis');
+insert into comments (comment_id, content, title, book_id, user_id) values (1,'seems Great', 'motivated', 5, 2);
+insert into comments (comment_id, content, title, book_id, user_id) values (2,'i Like it', 'grateful', 6, 1);
+insert into comments (comment_id, content, title, book_id, user_id) values (3,'Amazing book', 'Avis', 1,3);
+insert into comments (comment_id, content, title, book_id, user_id) values (4, 'i dislike its too long', 'too long', 2, 5);
+insert into comments (comment_id, content, title, book_id, user_id) values (5,'Good book', 'Avis', 4, 4);
